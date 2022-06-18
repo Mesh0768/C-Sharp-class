714220ad5849f287d7bbafea172c598979a87f17
Mesh0768 <mmeshack231@gmail.com>
2022-06-18 19:28:36 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int[]values={15,7,12,23,41,28,7,17,36};
+Console.WriteLine("Using Break and Continue");
+foreach(int val in values)
+{
+    if(val>=20 && val <= 29){
+        continue;
+    }
+    Console.WriteLine ("val is currently {0}",val);
+}
